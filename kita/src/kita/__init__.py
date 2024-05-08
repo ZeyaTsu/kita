9b08177b2f9fc9f89e5c29d1c04a24c822725ca6
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2024-05-08 16:02:33 +0200

    Add files via upload

--- kita/src/kita/__init__.py
+++ kita/src/kita/__init__.py
@@ -4,11 +4,16 @@ import inspect
 from .basic import *
 from .complex import *
 from .statistics import *
+from .vectors import *
 
 basic_functions = inspect.getmembers(basic, inspect.isfunction)
 complex_functions = inspect.getmembers(complex, inspect.isfunction)
 statistics_functions = inspect.getmembers(statistics, inspect.isfunction)
+vectors_functions = inspect.getmembers(vectors, inspect.isfunction)
 
-function_names = [name for name, _ in basic_functions + complex_functions + statistics_functions]
+vectors_classes = inspect.getmembers(vectors, inspect.isclass)
 
-__all__ = function_names
+function_names = [name for name, _ in basic_functions + complex_functions + statistics_functions + vectors_functions]
+class_names = [name for name, _ in vectors_classes]
+
+__all__ = function_names + class_names
