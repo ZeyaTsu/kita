1370671752fea1ff84c31f37a08df8a4360f2f5c
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2024-05-23 21:03:39 +0200

    Updated cos & sin based on Euler's equation
    Euler's equation stating that 
    cos(x) =  (e^(iθ) + e^(-iθ) )/2
    sin(x) =  (e^(iθ) - e^(-iθ) )/2

---
+++ kita/src/kita/trigonometry.py
@@ -0,0 +1,33 @@
+__pi = 3.141592653589793
+
+def __exp(x:complex):
+    return 2.718281828459045**x
+
+def __factorial(n:int):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+def deg(x):
+    rad = x * __pi/180
+    return rad
+
+def sin(x:float):
+    sinx = (__exp(x*1j) - __exp(x*-1j))/2
+    if sinx.imag == 0:
+        return sinx.real
+    elif sinx.real == 0:
+        return sinx.imag
+    return sinx
+
+def cos(x:float):
+    cosx = (__exp(x*1j) + __exp(x*-1j))/2
+    if cosx.imag == 0:
+        return cosx.real
+    elif cosx.real == 0:
+        return cosx.imag
+    return cosx
+
+def tan(x:float):
+    return sin(x)/cos(x)
